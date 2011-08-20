252938b63c2606c5d12882346ed3557c509761b4
Michael Davis <m.r.davis@me.com>
2011-08-20 11:42:12 +0000

    Remove paste and zopeskel from setup.py

--- setup.py
+++ setup.py
@@ -60,11 +60,3 @@ setup(name='oxford.homepage',
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
       test_suite='oxford.homepage.tests.test_docs.test_suite',
-      entry_points="""
-      # -*- entry_points -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
